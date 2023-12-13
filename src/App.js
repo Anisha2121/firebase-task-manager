d152b0872a9dd20c5ba6264dce69262398087acb
Anisha2121 <anisha.bha.jha@gmail.com>
2023-12-13 22:35:05 +0530

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import firebaseConfig from './config/firebaseConfig';
+import { initializeApp } from 'firebase/app';
+import { useEffect, useState } from 'react';
+
+initializeApp(firebaseConfig);
 
 function App() {
+
+  const navigate = useNavigate();
+  const [isUser, setUser] = useState(false);
+  const [isAdmin, setAdmin] = useState(false);
+
+  useEffect(() => {
+        if(isUser) {
+          navigate("/dashboard");
+        }
+    },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+          <Route exact path="/" element={<Login isUserProp={setUser}/>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
     </div>
   );
 }
