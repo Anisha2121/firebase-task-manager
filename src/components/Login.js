adae243d73d75c2f6f063d1453a3b60182f6670d
Anisha2121 <anisha.bha.jha@gmail.com>
2023-12-17 19:39:37 +0530

    ui edit

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,91 +5,83 @@ import { collection, getDocs, query, where } from 'firebase/firestore';
 import firestore from '../config/firebaseConfig';
 
 const Login = (props) => {
-    const [ email, setEmail ] = useState("");
-    const [ password, setPassword ] = useState("");
-    const [ loading, setLoading ] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    // useEffect(() => {
-    //     const token = localStorage.getItem('token');
+  const setUser = () => {
+    props.isUserProp(true);
+  }
 
-    //     if (token) {
-    //         history.push('/dashboard')
-    //     }
-    // },[history])
-    const setUser = () => {
-        props.isUserProp(true);
-    }
+  const setAdmin = () => {
+    props.isAdminProp(true);
+  }
 
-    const setAdmin = () => {
-        props.isAdminProp(true);
-    }
+  const onLogin = () => {
+    setLoading(true)
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        const user = userCredential.user;
+        const userId = user.uid;
+        try {
+          const queryStatement = query(collection(firestore, 'users'), where('uid', '==', userId));
+          const querySnapshot = await getDocs(queryStatement);
+          const usersRole = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          if (usersRole[0].role === "user") {
+            setUser();
+          } else if (usersRole[0].role === "admin") {
+            setAdmin();
+          }
+        } catch (e) {
+          alert(e.message);
+        }
+      })
+      .catch(e => alert(e.message))
+      .finally(() => setLoading(false))
+  }
 
-    const onLogin = () => {
-        setLoading(true)
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, email, password)
-            .then(async (userCredential) => {
-                const user = userCredential.user;
-                const userId = user.uid;
-                try {
-                    const queryStatement = query(collection(firestore, 'users'), where('uid', '==', userId));
-                    const querySnapshot = await getDocs(queryStatement);
-                    const usersRole = querySnapshot.docs.map((doc) => ({
-                        id: doc.id,
-                        ...doc.data(),
-                    }));
-                    if(usersRole[0].role === "user") {
-                        setUser();
-                    } else if(usersRole[0].role === "admin") {
-                        setAdmin();
-                    }
-                } catch (e) {
-                    alert(e.message);
-                }
-            })
-            .catch(e => alert(e.message))
-            .finally(() => setLoading(false))
-    }
-
-    return (
-        <div className="w-full h-screen bg-gradient-to-r from-yellow-200 via-red-500 to-pink-500 flex justify-center items-center">
-            <div className="w-96 bg-white shadow-lg">
-                <div className="m-5">
-                    <label className="block text-xl font-bold mb-2">Email</label>
-                    <input
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        name="email"
-                        type="email"
-                        className="border-grey-200 border-2 rounded w-full p-2 h-10"
-                    />
-                </div>
-                <div className="m-5">
-                    <label className="block text-xl font-bold mb-2">Password</label>
-                    <input
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        name="password"
-                        type="password"
-                        className="border-grey-200 border-2 rounded w-full p-2 h-10"
-                    />
-                </div>
-                <div className="m-5">
-                    <button
-                        onClick={onLogin}
-                        className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-200 text-white px-10 py-2 rounded text-xl font-bold"
-                    >
-                        {loading ? 'Logging you in ...' : 'Login'}
-                    </button>
-                </div>
-                <div className="m-5">
-                    <Link to="/signup">
-                        Don't have an account?
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <div className="w-96 bg-white shadow-lg p-8 rounded-md">
+        <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Email</label>
+          <input
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            name="email"
+            type="email"
+            className="border-2 rounded w-full p-2"
+          />
         </div>
-    )
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Password</label>
+          <input
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            name="password"
+            type="password"
+            className="border-2 rounded w-full p-2"
+          />
+        </div>
+        <div className="mb-4">
+          <button
+            onClick={onLogin}
+            className="bg-blue-500 text-white px-10 py-2 rounded text-xl font-bold w-full"
+          >
+            {loading ? 'Logging you in ...' : 'Login'}
+          </button>
+        </div>
+        <div className="text-center">
+          <Link to="/signup" className="text-blue-500">Don't have an account?</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,96 +4,92 @@ import { Link } from 'react-router-dom';
 import { collection, addDoc } from 'firebase/firestore';
 import firestore from '../config/firebaseConfig';
 
+const Signup = (props) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-const Signup = (props ) => {
-    const [ name, setName ] = useState("");
-    const [ email, setEmail ] = useState("");
-    const [ password, setPassword ] = useState("");
-    const [ loading, setLoading ] = useState(false);
+  const setUser = () => {
+    props.isUserProp(true);
+  }
 
-    const setUser = () => {
-        props.isUserProp(true);
+  const onSignup = async () => {
+    setLoading(true);
+    const auth = getAuth();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      const userId = user.uid;
+
+      const docRef = await addDoc(collection(firestore, 'users'), {
+        uid: userId,
+        role: "user",
+        name: name,
+      });
+
+      const docId = docRef.id;
+      if (docId) {
+        setUser();
+      } else {
+        console.log("doc id is null");
+      }
+    } catch (e) {
+      console.error('Error creating user: ', e.message);
+      alert(e.message);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    const onSignup = () => {
-        setLoading(true);
-        const auth = getAuth();
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(async (userCredential) => {
-            const user = userCredential.user;
-            const userId = user.uid;
-            try {
-                const docRef = await addDoc(collection(firestore, 'users'), {
-                  uid: userId,
-                  role: "user",
-                  name: name,
-                });
-                const docId = docRef.id;
-                if(docId) {
-                    setUser();
-                } else {
-                    console.log("doc id is null");
-                }
-              } catch (e) {
-                console.error('Error adding document: ', e);
-              }
-        })
-        .catch((e) => {
-            alert(e.message);
-            setLoading(false);
-        });
-    }
-
-
-    return (
-        <div className="w-full h-screen bg-gradient-to-r from-yellow-200 via-red-500 to-pink-500 flex justify-center items-center">
-            <div className="w-96 bg-white shadow-lg">
-                <div className="m-5">
-                    <label className="block text-xl font-bold mb-2">Name</label>
-                    <input
-                        value={name}
-                        onChange={e => setName(e.target.value)}
-                        name="name"
-                        type="name"
-                        className="border-grey-200 border-2 rounded w-full p-2 h-10"
-                    />
-                </div>
-                <div className="m-5">
-                    <label className="block text-xl font-bold mb-2">Email</label>
-                    <input
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        name="email"
-                        type="email"
-                        className="border-grey-200 border-2 rounded w-full p-2 h-10"
-                    />
-                </div>
-                <div className="m-5">
-                    <label className="block text-xl font-bold mb-2">Password</label>
-                    <input
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        name="password"
-                        type="password"
-                        className="border-grey-200 border-2 rounded w-full p-2 h-10"
-                    />
-                </div>
-                <div className="m-5">
-                    <button
-                        onClick={onSignup}
-                        className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-200 text-white px-10 py-2 rounded text-xl font-bold"
-                    >
-                        { loading ? 'Creating user ...' : 'Signup'}
-                    </button>
-                </div>
-                <div className="m-5">
-                    <Link to="/">
-                        Already have an account?
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <div className="w-96 bg-white shadow-lg p-8 rounded-md">
+        <h1 className="text-2xl font-bold mb-6 text-center">Sign Up</h1>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Name</label>
+          <input
+            value={name}
+            onChange={e => setName(e.target.value)}
+            name="name"
+            type="name"
+            className="border-2 rounded w-full p-2"
+          />
         </div>
-    )
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Email</label>
+          <input
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            name="email"
+            type="email"
+            className="border-2 rounded w-full p-2"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Password</label>
+          <input
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            name="password"
+            type="password"
+            className="border-2 rounded w-full p-2"
+          />
+        </div>
+        <div className="mb-4">
+          <button
+            onClick={onSignup}
+            className="bg-blue-500 text-white px-10 py-2 rounded text-xl font-bold w-full"
+          >
+            {loading ? 'Creating user ...' : 'Signup'}
+          </button>
+        </div>
+        <div className="text-center">
+          <Link to="/" className="text-blue-500">Already have an account?</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
