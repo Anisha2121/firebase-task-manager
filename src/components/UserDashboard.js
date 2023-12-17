397902884e7b3965b139def2813b476f8b6ea9bf
Anisha2121 <anisha.bha.jha@gmail.com>
2023-12-17 21:36:40 +0530

    user dashboard remark edit

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -17,6 +17,7 @@ const UserDashboard = (props) => {
   const [userTasks, setUserTasks] = useState([]);
   const [editingTaskId, setEditingTaskId] = useState(null);
   const [editedStatus, setEditedStatus] = useState('');
+  const [editedRemark, setEditedRemark] = useState('');
 
   const setUser = () => {
     props.isUserProp(false);
@@ -33,37 +34,73 @@ const UserDashboard = (props) => {
 
   const auth = getAuth();
 
-  const handleEditClick = (taskId, currentStatus) => {
+  const handleEditClick = (taskId, currentStatus, currentRemark) => {
     setEditingTaskId(taskId);
     setEditedStatus(currentStatus);
+    setEditedRemark(currentRemark);
   };
+
+  const handleRemarkClick = (taskId, currentRemark) => {
+    setEditingTaskId(taskId);
+    setEditedRemark(currentRemark);
+  };
+
+  const handleRemarkChange = (newRemark) => {
+    setEditedRemark(newRemark);
+  };
+
   const handleStatusChange = (newStatus) => {
     setEditedStatus(newStatus);
   };
 
   const handleSaveClick = async (taskId) => {
     try {
-      // Update the status in the Firestore database
       const taskRef = doc(firestore, 'tasks', taskId);
-      await updateDoc(taskRef, {
-        status: editedStatus
-      });
 
-      // Update the local state with the edited status
-      setUserTasks((prevTasks) =>
-        prevTasks.map((task) =>
-          task.id === taskId ? {
-            ...task,
-            status: editedStatus
-          } : task
-        )
-      );
+      if (editedStatus && editingTaskId) {
+        // Update the status in the Firestore database
+        await updateDoc(taskRef, {
+          status: editedStatus,
+        });
+
+        // Update the local state with the edited status
+        setUserTasks((prevTasks) =>
+          prevTasks.map((task) =>
+            task.id === taskId
+              ? {
+                  ...task,
+                  status: editedStatus,
+                }
+              : task
+          )
+        );
+      }
+
+      if (editedRemark && editingTaskId) {
+        // Update the remark in the Firestore database
+        await updateDoc(taskRef, {
+          remark: editedRemark,
+        });
+
+        // Update the local state with the edited remark
+        setUserTasks((prevTasks) =>
+          prevTasks.map((task) =>
+            task.id === taskId
+              ? {
+                  ...task,
+                  remark: editedRemark,
+                }
+              : task
+          )
+        );
+      }
 
       // Clear the editing state
       setEditingTaskId(null);
       setEditedStatus('');
+      setEditedRemark('');
     } catch (error) {
-      console.error('Error updating task status:', error.message);
+      console.error('Error updating task status and remark:', error.message);
     }
   };
 
@@ -76,7 +113,7 @@ const UserDashboard = (props) => {
         const usersSnapshot = await getDocs(userQuery);
         const usersData = usersSnapshot.docs.map((doc) => ({
           id: doc.id,
-          ...doc.data()
+          ...doc.data(),
         }));
 
         const tasksCollection = collection(firestore, 'tasks');
@@ -84,7 +121,7 @@ const UserDashboard = (props) => {
         const userTasksSnapshot = await getDocs(userTasksQuery);
         const userTasksData = userTasksSnapshot.docs.map((doc) => ({
           id: doc.id,
-          ...doc.data()
+          ...doc.data(),
         }));
 
         setUserTasks(userTasksData);
@@ -98,81 +135,109 @@ const UserDashboard = (props) => {
 
   return (
     <>
-    <button
-    onClick={logout}
-    className="bg-blue-500 text-white px-5 py-2 rounded-semi text-lg font-bold mb-6 w-small"
-    style={{ position: 'absolute', top: 0, right: 0 }}
-  >
-    Logout
-  </button>
-    <div className="w-full h-screen bg-gray-100 flex flex-col">
-      <header className="bg-blue-500 text-white py-2">
-        <div className="container mx-auto">
-          <h1 className="text-2xl font-bold">User Dashboard</h1>
-        </div>
-      </header>
-      <main className="flex-1 container mx-auto mt-1 flex justify-center">
-  <div className="bg-white p-4 rounded shadow mt-4 h-auto"> 
-    <table className="w-full border-collapse border border-gray-300">
-                <thead>
-                  <tr className="bg-blue-500 text-white">
-                    <th className="py-2 px-4">Project ID</th>
-                    <th className="py-2 px-4">Task Name</th>
-                    <th className="py-2 px-4">Start Date</th>
-                    <th className="py-2 px-4">End Date</th>
-                    <th className="py-2 px-4">Priority</th>
-                    <th className="py-2 px-4">Status</th>
-                    <th className="py-2 px-4">Assignee</th>
-                    <th className="py-2 px-4">Actions</th>
+      <button
+        onClick={logout}
+        className="bg-blue-500 text-white px-5 py-2 rounded-semi text-lg font-bold mb-6 w-small"
+        style={{ position: 'absolute', top: 0, right: 0 }}
+      >
+        Logout
+      </button>
+      <div className="w-full h-screen bg-gray-100 flex flex-col">
+        <header className="bg-blue-500 text-white py-2">
+          <div className="container mx-auto">
+            <h1 className="text-2xl font-bold">User Dashboard</h1>
+          </div>
+        </header>
+        <main className="flex-1 container mx-auto mt-1 flex justify-center">
+          <div className="bg-white p-4 rounded shadow mt-4">
+            <table className="w-full border-collapse border border-gray-300">
+              <thead>
+                <tr className="bg-blue-500 text-white">
+                  <th className="py-2 px-4">Project ID</th>
+                  <th className="py-2 px-4">Task Name</th>
+                  <th className="py-2 px-4">Start Date</th>
+                  <th className="py-2 px-4">End Date</th>
+                  <th className="py-2 px-4">Priority</th>
+                  <th className="py-2 px-4">Status</th>
+                  <th className="py-2 px-4">Assignee</th>
+                  <th className="py-2 px-4">Remark</th>
+                  <th className="py-2 px-4">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userTasks.map((task) => (
+                  <tr key={task.id} className="text-center">
+                    <td className="border py-2 px-4">{task.projectId}</td>
+                    <td className="border py-2 px-4">{task.taskName}</td>
+                    <td className="border py-2 px-4">{task.startDate}</td>
+                    <td className="border py-2 px-4">{task.endDate}</td>
+                    <td className="border py-2 px-4">{task.priority}</td>
+                    <td className="border py-2 px-4">
+                      {editingTaskId === task.id ? (
+                        <select
+                          value={editedStatus}
+                          onChange={(e) => handleStatusChange(e.target.value)}
+                        >
+                          <option value="In Progress">In Progress</option>
+                          <option value="Completed">Completed</option>
+                          <option value="On Hold">On Hold</option>
+                          <option value="to do">To Do</option>
+                        </select>
+                      ) : (
+                        task.status
+                      )}
+                    </td>
+                    <td className="border py-2 px-4">{task.assignee}</td>
+                    <td className="border py-2 px-4">
+                      {task.endDate < new Date().toISOString() ? (
+                        <>
+                          {editingTaskId === task.id ? (
+                            <textarea
+                              value={editedRemark}
+                              onChange={(e) => handleRemarkChange(e.target.value)}
+                              className="border rounded p-1 w-full"
+                              rows={3}
+                            />
+                          ) : (
+                            task.remark
+                          )}
+                          {!task.remark && !editingTaskId && (
+                            <button
+                              onClick={() => handleRemarkClick(task.id, task.remark)}
+                              className="bg-blue-500 text-white px-2 py-1 rounded ml-2"
+                              disabled={editingTaskId === task.id}
+                            >
+                              Add Remarks
+                            </button>
+                          )}
+                        </>
+                      ) : (
+                        task.remark
+                      )}
+                    </td>
+                    <td className="border py-2 px-4">
+                      <button
+                        onClick={() => handleEditClick(task.id, task.status, task.remark)}
+                        className="bg-blue-500 text-white px-2 py-1 rounded"
+                      >
+                        Edit 
+                      </button>
+                      {editingTaskId === task.id && (
+                        <button
+                          onClick={() => handleSaveClick(task.id)}
+                          className="bg-green-500 text-white px-2 py-1 rounded ml-2"
+                        >
+                          Save
+                        </button>
+                      )}
+                    </td>
                   </tr>
-                </thead>
-                <tbody>
-                  {userTasks.map((task) => (
-                    <tr key={task.id} className="text-center">
-                      <td className="border py-2 px-4">{task.projectId}</td>
-                      <td className="border py-2 px-4">{task.taskName}</td>
-                      <td className="border py-2 px-4">{task.startDate}</td>
-                      <td className="border py-2 px-4">{task.endDate}</td>
-                      <td className="border py-2 px-4">{task.priority}</td>
-                      <td className="border py-2 px-4">
-                        {editingTaskId === task.id ? (
-                          <select
-                            value={editedStatus}
-                            onChange={(e) => handleStatusChange(e.target.value)}
-                          >
-                            <option value="In Progress">In Progress</option>
-                            <option value="Completed">Completed</option>
-                            <option value="On Hold">On Hold</option>
-                            <option value="to do">To Do</option>
-                          </select>
-                        ) : (
-                          task.status
-                        )}
-                      </td>
-                      <td className="border py-2 px-4">{task.assignee}</td>
-                      <td className="border py-2 px-4">
-  <button
-    onClick={() => handleEditClick(task.id, task.status)}
-    className="bg-blue-500 text-white px-2 py-1 rounded"
-  >
-    Edit
-  </button>
-  {editingTaskId === task.id && (
-    <button
-      onClick={() => handleSaveClick(task.id)}
-      className="bg-green-500 text-white px-2 py-1 rounded ml-2"
-    >
-      Save
-    </button>
-  )}
-</td>
-                    </tr>
-                  ))}
-                </tbody>
-                </table>
-    </div>
-  </main>
-</div>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
     </>
   );
 };
